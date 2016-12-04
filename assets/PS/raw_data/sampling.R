3ae7537e4bf488edb25bb4580659638c0d81c65f
rudeboybert <albert.ys.kim@gmail.com>
2016-12-04 07:26:27 -0500

    blah

--- assets/PS/raw_data/sampling.R
+++ assets/PS/raw_data/sampling.R
@@ -1,8 +1,15 @@
-get_sample_of_congress <- function(n, seed){
-  set.seed(seed)
+get_sample_of_congress <- function(n){
   url <- "https://rudeboybert.github.io/MATH116/assets/PS/raw_data/congress.csv"
   congress <- suppressMessages(readr::read_csv(url))
-  sample_congress <- dplyr::sample_n(congress, n)
-  return(sample_congress)
+  sample_of_congress <- dplyr::sample_n(congress, n)
+  return(sample_of_congress)
+}
+
+get_sample_of_congress_113 <- function(n){
+  url <- "https://rudeboybert.github.io/MATH116/assets/PS/raw_data/congress.csv"
+  congress_113 <- suppressMessages(readr::read_csv(url)) %>% 
+    filter(congress==113)
+  sample_of_congress_113 <- dplyr::sample_n(congress_113, n)
+  return(sample_of_congress_113)
 }
 
