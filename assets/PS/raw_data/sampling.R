d5fad658aff4882d3291d7a9423fa4ae3c7a5f2a
rudeboybert <albert.ys.kim@gmail.com>
2016-12-04 07:43:18 -0500

    blah

--- assets/PS/raw_data/sampling.R
+++ assets/PS/raw_data/sampling.R
@@ -1,6 +1,7 @@
 get_sample_of_congress <- function(n){
   url <- "https://rudeboybert.github.io/MATH116/assets/PS/raw_data/congress.csv"
   congress <- suppressMessages(readr::read_csv(url))
+  congress <- dplyr::select(congress, -c(bioguide, middlename, suffix))
   
   sample_of_congress <- dplyr::sample_n(congress, n)
   return(sample_of_congress)
@@ -9,6 +10,7 @@ get_sample_of_congress <- function(n){
 get_sample_of_congress_then_and_now <- function(n_80, n_113){
   url <- "https://rudeboybert.github.io/MATH116/assets/PS/raw_data/congress.csv"
   congress <- suppressMessages(readr::read_csv(url))
+  congress <- dplyr::select(congress, -c(bioguide, middlename, suffix))
   
   congress_80 <- dplyr::filter(congress, congress==80)
   congress_80 <- dplyr::sample_n(congress, n_80)
@@ -21,6 +23,7 @@ get_sample_of_congress_then_and_now <- function(n_80, n_113){
 get_sample_of_congress_113 <- function(n){
   url <- "https://rudeboybert.github.io/MATH116/assets/PS/raw_data/congress.csv"
   congress <- suppressMessages(readr::read_csv(url)) 
+  congress <- dplyr::select(congress, -c(bioguide, middlename, suffix))
   
   congress_113 <- dplyr::filter(congress, congress==113)
   sample_of_congress_113 <- dplyr::sample_n(congress_113, n)
